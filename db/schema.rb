1c239debc76489866c0e184b98e72efeb28b1e4c
JIGAR KHANNA <khanna.jigar23@gmail.com>
2021-11-09 18:30:16 +0530

    form helpers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_121313) do
+ActiveRecord::Schema.define(version: 2021_11_09_105702) do
 
   create_table "libraries", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2021_11_08_121313) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "message"
+    t.date "pub_date"
+    t.string "address"
+    t.integer "phone"
+    t.string "color"
   end
 
 end
