3f607af6a1c13e57c1f4088a57cb3465677cf6fb
Ankit Rawal <18305@iiitu.ac.in>
2021-11-09 16:06:08 +0530

    add callbacks,bootstrap btns and tables

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,2 +1,16 @@
 class Library < ApplicationRecord
+    # Callbacks
+    before_save :merge_data
+    after_destroy :update_log
+
+    def merge_data
+        self.name = "Title : #{self.name}"
+        self.author = "Mr/Mrs #{self.author}"
+        self.price = "#{self.price} Rs"
+     end 
+
+    def update_log
+        logger.info "------------- Book has been Deleted #{self.name}!"
+    end
+    
 end
